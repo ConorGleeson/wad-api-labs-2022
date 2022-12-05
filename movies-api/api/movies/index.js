ccdcbd37464044d0618943a46094e35732824d44
Conor Gleeson <74910941+ConorGleeson@users.noreply.github.com>
2022-12-05 18:20:43 +0000

    Topic 11 - I've completed all my labs

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
+import {
+    getUpcomingMovies
+  } from '../tmdb-api';
 
 
 
@@ -57,6 +60,11 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 
 
